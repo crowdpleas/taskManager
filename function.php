e35e37fc0b4231ce71eecbca76c1aa6ee8172ded
crowdpleas <slivkadima003@gmail.com>
2021-11-21 22:13:29 +0200

    clean work

--- function.php
+++ function.php
@@ -4,18 +4,18 @@ $multiply = 0;
 $res = 0;
 function getSumm($number1, $number2)
 {
-    return ($number1 + $number2);
+    return $number1 + $number2;
 }
 
 function getMultiply($number3, $summ)
 {
-    return ($summ * $number3);
+    return $summ * $number3;
 }
 
 // Hаписати функцію яка приймає результати цих фунцій робить шось з ними я вибрав вычитание от второго первое
 function getRes($multiply, $summ)
 {
-    echo($multiply - $summ);
+    echo $multiply - $summ;
 }
 
 $number1 = 6;
