ada4e3c3a3ea42fc4b329807f7bcf9db2a5e1e59
crowdpleas <slivkadima003@gmail.com>
2021-11-21 21:46:35 +0200

    vork func

--- function.php
+++ function.php
@@ -1,12 +1,23 @@
 <?php
-    function getSumm(){
-        $summ=$number1+$number2;
-    }
-    function getMultiply(){
-        $multiply=$summ*$number3;
-    }
+function getSumm($number1,$number2){
+    $summ = 0;
+    $summ=$number1+$number2;
+    return $summ;
+}
+function getMultiply($number3,$summ){
+    $multiply = 0;
+    $multiply=$summ*$number3;
+    return $multiply;
+}
 // Hаписати функцію яка приймає результати цих фунцій робить шось з ними я вибрав вычитание от второго первое
-    function getResult(){
-        $result=$multiply-$summ;
-        return $result;
-    }
+function getRes($multiply,$summ){
+    $res = 0;
+    $res=$multiply-$summ;
+    echo $res;
+}
+$number1 = 4;
+$number2 = 6;
+$summ = getSumm($number1,$number2);
+$number3 = 2;
+$multiply = getMultiply($number3,$summ);
+$res = getRes($multiply,$summ);
