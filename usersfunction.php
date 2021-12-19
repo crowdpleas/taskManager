60b0c66dcb9f754f4b5df4ae6ce3d0b1172f6e99
crowdpleas <slivkadima003@gmail.com>
2021-12-20 00:13:45 +0200

    func with edit user

--- usersfunction.php
+++ usersfunction.php
@@ -14,31 +14,30 @@ function deleteUser(&$arr,$delIndex,$lenght)
 {
       return array_splice($arr,$delIndex,$lenght);
 }
-//function editUser($arr, $key, $name, $age, $job){
-$arr2 = [['name'=>'vasya','age'=>25,'job'=>'bomj'],
-         ['name'=>'volodya','age'=>31,'job'=>'student'],
-         ['name'=>'viktor','age'=>19,'job'=>'teacher']];
-function editUser(&$arr2, $id, $name, $age, $job){
-    $arr2[$id] = ['name' => $name,'age'=>$age,'job'=>$job];
-    //$arr2[$id] = ['age'=>$age];
-    //$arr2[$id] = ['job'=>$job];
+$arr2 = [['name'=>'dimka_goshta','age'=>19,'job'=>'bussinesment'],
+    ['name'=>'volodya','age'=>31,'job'=>'student'],
+    ['name'=>'viktor','age'=>19,'job'=>'teacher']];
+function editUser($arr, $id, $name, $age, $job = null)
+{
+    if ($age >= 18 && $job != null){
+        $arr[$id] = ['name' => $name,'age' => $age,'job' => $job];
+        return $arr = $arr;
+    }
+    $arr[$id] = ['name' => $name, 'age' => $age];
+    return $arr = $arr;
 }
-
 $arr[] = newUser('Dimka', 19, 'zavod');
-$arr[] = newUser('Oleg',17,'bussinesment');
+$arr[] = newUser('Oleg',19,'bussinesment');
 //deleteUser($arr, 2,1);
 //$arr[] = editUser($arr, 1, 'dima',18,'zavod');
 echo "<pre>";
+$arr = editUser($arr,3,'dimka',16,'teacher');
 print_r($arr);
 echo "</pre>";
 echo "<pre>";
-print_r($arr[2]);
+//print_r($arr2);
 echo "</pre>";
 echo "<pre>";
-print_r($arr2);
+//$arr2 = editUser($arr2, 1, 'Sasha', 19);
+//print_r($arr2);
 echo "</pre>";
-echo "<pre>";
-$arr2[] = editUser($arr2, 1,'oleg',25,'reporter');
-print_r($arr2);
-echo "</pre>";
-;
